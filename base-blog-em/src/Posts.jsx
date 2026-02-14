5b8ef8a730e9858237f5143d62d597ea21093518
Ital023 <contadoitalo911@gmail.com>
2026-02-13 22:38:06 -0300

    feat: enhance error handling and loading state in Posts component

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -3,17 +3,21 @@ import { useState } from "react";
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 import { useQuery } from "@tanstack/react-query";
+
 const maxPostPage = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isError, error, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts
   });
-  if (!data) return <div />
+
+  if (isLoading) return <h3>Loading...</h3>
+
+  if (isError) return <><h3>Oops, something went wrong</h3><p>{error.message}</p></>
 
   return (
     <>

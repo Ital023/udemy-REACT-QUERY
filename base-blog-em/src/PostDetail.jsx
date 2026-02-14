fa8b66edccaf8d7cf13e4b4fb3c8047c65554f87
Ital023 <contadoitalo911@gmail.com>
2026-02-14 09:44:05 -0300

    feat: add delete functionality to PostDetail component

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -2,21 +2,27 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
-export function PostDetail({ post }) {
-  
+export function PostDetail({ post, deleteMutation }) {
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["comments", post.id],
-    queryFn: () => fetchComments(post.id)
+    queryFn: () => fetchComments(post.id),
   });
 
-  if (isLoading) return <h3>Loading...</h3>
+  if (isLoading) return <h3>Loading...</h3>;
 
-  if (isError) return <><h3>Oops, something went wrong</h3><p>{error.message}</p></>
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.message}</p>
+      </>
+    );
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const maxPostPage = 10;
 
@@ -11,6 +11,10 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  });
 
   useEffect(() => {
     if (currentPage < maxPostPage) {
@@ -70,7 +74,7 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} />}
     </>
   );
 }
